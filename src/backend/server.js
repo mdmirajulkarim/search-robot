a6ba5acb808de8b40d06f926e42e6f078429ec46
karim <karim@csit.fi>
2020-04-28 23:27:02 +0300

    api

---
+++ src/backend/server.js
@@ -0,0 +1,58 @@
+export const robots = [
+    {
+        id:1,
+        name:'alien',
+        username:'alien',
+        email:'alien@gmail.com'
+  
+    },
+    {
+        id:2,
+        name:'Snowden',
+        username:'snowden',
+        email:'snowden@gmail.com'
+  
+    },
+    {
+        id:3,
+        name:'Iron Man',
+        username:'iRON',
+        email:'iron@gmail.com'
+  
+    },
+    {
+        id:4,
+        name:'The Batman',
+        username:'Batman',
+        email:'batman@gmail.com'
+  
+    },
+    {
+        id:5,
+        name:'SpiderMan',
+        username:'Spider',
+        email:'spider@gmail.com'
+  
+    },
+    {
+        id:6,
+        name:'the Robo',
+        username:'Robot',
+        email:'Robo@gmail.com'
+  
+    },
+    {
+        id:7,
+        name:'the Rock',
+        username:'Rock',
+        email:'rock@gmail.com'
+  
+    },
+    {
+        id:8,
+        name:'the Hitman',
+        username:'Hitman',
+        email:'hitman@gmail.com'
+  
+    },
+  ]
