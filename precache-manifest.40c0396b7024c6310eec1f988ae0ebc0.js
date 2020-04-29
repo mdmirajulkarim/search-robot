0c960ad85449e02551df8b3fbb13aa423070de12
karim <karim@csit.fi>
2020-04-29 23:39:33 +0300

    Updates

---
+++ precache-manifest.40c0396b7024c6310eec1f988ae0ebc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96c82b3ebf37b0d834e8311c8bf12e5",
+    "url": "/search-robot/index.html"
+  },
+  {
+    "revision": "29d5f4f0065a7336acc6",
+    "url": "/search-robot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e0c8f15d7ba018b6b5ac",
+    "url": "/search-robot/static/css/main.997fd0d6.chunk.css"
+  },
+  {
+    "revision": "29d5f4f0065a7336acc6",
+    "url": "/search-robot/static/js/2.ab1898a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-robot/static/js/2.ab1898a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c8f15d7ba018b6b5ac",
+    "url": "/search-robot/static/js/main.34343dbb.chunk.js"
+  },
+  {
+    "revision": "c0eaac6960cd498ff3bc",
+    "url": "/search-robot/static/js/runtime-main.88488bee.js"
+  }
+]);
